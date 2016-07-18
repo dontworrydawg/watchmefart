0cc93acbd1ec0397125f70b3884713def551a543
stepandel <stepandel@gmail.com>
2016-07-18 09:32:30 -0400

    Add files via upload

---
+++ InterfaceController.h
@@ -0,0 +1,15 @@
+//
+//  InterfaceController.h
+//  Watch Extension
+//
+//  Created by Ber Jr on 2016-07-13.
+//  Copyright © 2016 Ber Jr. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchMeFart
+//
+//  Created by Ber Jr on 2016-07-13.
+//  Copyright © 2016 Ber Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
