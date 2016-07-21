97e04a76c4e374ca9bd5a5e083af7ddd3ad0d822
stepandel <stepandel@gmail.com>
2016-07-20 21:15:02 -0400

    Add files via upload

---
+++ WatchMeFart/Watch Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  Watch Extension
+//
+//  Created by Ber Jr on 2016-07-13.
+//  Copyright © 2016 Ber Jr. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
---
+++ WatchMeFart/WatchMeFart/ToFart.m
@@ -0,0 +1,13 @@
+//
+//  ToFart.m
+//  WatchMeFart
+//
+//  Created by Ber Jr on 2016-07-15.
+//  Copyright © 2016 Ber Jr. All rights reserved.
+//
+
+#import "ToFart.h"
+
+@implementation ToFart
+
+@end
